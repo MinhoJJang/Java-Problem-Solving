fee1a24767ddd7e343b4337d69ae428492d334b6
MinhoJJang <ahchjang@naver.com>
2024-02-28 23:53:48 +0900

    [SOLVED] 아스키 코드 [B5]

---
+++ BOJ/Else/src/BOJ_11654_B5/Main.java
@@ -0,0 +1,11 @@
+package BOJ_11654_B5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextLine().toCharArray()[0];
+        System.out.printf("%d", k);
+    }
+}
