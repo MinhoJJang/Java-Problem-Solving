87a0b894d4d237a0fd859214941ed7329e4a59cf
MinhoJJang <ahchjang@naver.com>
2024-03-11 13:08:21 +0900

    [SOLVED] 그대로 출력하기 2 [B3]

---
+++ BOJ/LaTrobeAlgorithm/src/week1/BOJ_11719_B3/Main.java
@@ -0,0 +1,21 @@
+package week1.BOJ_11719_B3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+
+        br.close();
+    }
+}
+
+
