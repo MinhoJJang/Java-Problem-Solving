27dd0e64025916bb1250c7798731b8521a6ba6c2
MinhoJJang <ahchjang@naver.com>
2024-03-11 13:09:07 +0900

    [SOLVED] A+B -4 [B5]

---
+++ BOJ/LaTrobeAlgorithm/src/week1/BOJ_10951_B5/Main.java
@@ -0,0 +1,16 @@
+package week1.BOJ_10951_B5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextInt()) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A + B);
+        }
+    }
+}
+
