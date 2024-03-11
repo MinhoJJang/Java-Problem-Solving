e82b11b78c3af349514ce43972b506e541794e61
MinhoJJang <ahchjang@naver.com>
2024-03-11 13:09:17 +0900

    [SOLVED] 고양이 [B5]

---
+++ BOJ/LaTrobeAlgorithm/src/week1/BOJ_10171_B5/Main.java
@@ -0,0 +1,10 @@
+package week1.BOJ_10171_B5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
